0fc774678ef0a91f01d1b12dc9f5725b86ba9c57
Lewis Herbert <99.lewish@gmail.com>
2022-05-16 11:05:53 +0100

    added parenthesis

--- src/main/java/com/hzy/cmbcalc/CombatLevelCalculator.java
+++ src/main/java/com/hzy/cmbcalc/CombatLevelCalculator.java
@@ -10,8 +10,8 @@ public class CombatLevelCalculator {
     public double calculateLevel(int[] levels) {
         double base = 0.25 * (levels[2] + levels[3] + (levels[6] / 2));
         double melee = xratio * (levels[0] + levels[1]);
-        double range = xratio * (levels[4] / 2) + levels[4];
-        double mage = xratio * (levels[5] / 2) + levels[5];
+        double range = xratio * ((levels[4] / 2) + levels[4]);
+        double mage = xratio * ((levels[5] / 2) + levels[5]);
         double level = base + Math.max(Math.max(range, mage), melee);
         return level;
     }
